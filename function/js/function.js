e4255c70675c6d39abeab2b3803c6acb6406badf
jayjay0515 <jayberona15@gmail.com>
2026-01-30 12:09:57 +0800

    Add files via upload

---
+++ function/js/function.js
@@ -0,0 +1,15 @@
+
+/*function normal('jay','jhbdfbw'){
+    withparameter(fname,lname);
+}*/
+    /*const sum = getsum(11,20);
+    alert(sum)*/
+
+function withparameter(fname,lname){
+    alert("hello"+" " + fname + " "+ lname);
+}
+
+/*function getsum(num1,num2){
+    const sum =num1 +num2;
+    return sum;
+}*/
